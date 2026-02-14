7dc83b223350c8282d2494741f67ffe6d2fc14af
Anjana B <155134341+Anjana-05@users.noreply.github.com>
2026-02-14 22:07:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.java
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.java
@@ -1,11 +1,38 @@
 class Solution {
-    int countFreq(int[] arr, int target) {
-        // code here
-        int c = 0;
-        for(int i = 0 ; i < arr.length ;i++){
-            if(arr[i] == target)
-                c++;
+    int firstIndex(int[] arr, int low, int high, int target){
+        int pos = arr.length;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(arr[mid] >= target){
+                pos = mid;
+                high = mid - 1;
+            }
+            else
+                low = mid + 1;
         }
-        return c;
+        return pos;
+    }
+    
+    int lastIndex(int[] arr, int low, int high,int target){
+        int pos = arr.length;
+        while(low <= high){
+            int mid = (low + high) /2;
+            if(arr[mid] > target){
+                pos = mid;
+                high = mid - 1;
+            }
+            else
+                low = mid + 1;
+        }
+        return pos;
+    }
+    
+    int countFreq(int[] arr, int target) {
+        int low = 0, high = arr.length-1;
+        
+        int lowerBound = firstIndex(arr, low, high, target);
+        int upperBound = lastIndex(arr, low, high, target);
+        
+        return upperBound - lowerBound;
     }
 }
