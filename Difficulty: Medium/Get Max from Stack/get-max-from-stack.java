da4a249cf602320fec2c4d0196c1fe84431b4b65
Anjana B <155134341+Anjana-05@users.noreply.github.com>
2026-08-02 15:00:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Get Max from Stack/get-max-from-stack.java
+++ Difficulty: Medium/Get Max from Stack/get-max-from-stack.java
@@ -1,42 +1,56 @@
 class SpecialStack {
-    Stack<Integer> stack; 
-    Stack<Integer> maxStack;
-    public SpecialStack() {
-        stack = new Stack<>();
-        maxStack = new Stack<>();
-    }
+	Stack<Long> stack;
+	long max;
+	public SpecialStack() {
+		stack = new Stack<>();
+		max = Long.MIN_VALUE;
+	}
+	
+	public void push(int x) {
+		long val = (long)x;
+		
+		if (stack.isEmpty()) {
+			stack.push(val);
+			max = val;
+		}
+		
+		else {
+			if (val > max) {
+				long curr = 2 * val - max;
+				stack.push(curr);
+				max = val;
+			}
+			else
+			    stack.push(val);
+		}
+		
+	}
+	
+	public void pop() {
+	    if(stack.isEmpty()) return;
+		// Remove the top element from the Stack
+		if (stack.peek() > max) {
+			max = 2 * max - stack.peek();
+		}
+		stack.pop();
+	}
+	
+	public int peek() {
+		// Returns top element of the Stack
+		if(stack.isEmpty()) return -1;
+		long x = stack.peek();
+		return x > max ? (int)max : (int)x;
+	}
+	
+	boolean isEmpty() {
+		// Check if the stack is empty
+		return stack.isEmpty();
+	}
+	
+	public int getMax() {
+		// Finds maximum element of Stack
+		if(stack.isEmpty()) return -1;
 
-    public void push(int x) {
-        stack.push(x);
-        
-        if(maxStack.isEmpty() || x >= maxStack.peek())
-            maxStack.push(x);
-        
-    }
-
-    public void pop() {
-        if(stack.isEmpty()) return;
-        // Remove the top element from the Stack
-        if(stack.peek().equals(maxStack.peek())){
-            maxStack.pop();
-        }
-        stack.pop();
-    }
-
-    public int peek() {
-        // Returns top element of the Stack
-        if(stack.isEmpty()) return -1;
-        return stack.peek();
-    }
-
-    boolean isEmpty() {
-        // Check if the stack is empty
-        return stack.isEmpty();
-    }
-
-    public int getMax() {
-        // Finds maximum element of Stack
-        if(maxStack.isEmpty()) return -1;
-        return maxStack.peek();
-    }
+		return (int)max;
+	}
 }
